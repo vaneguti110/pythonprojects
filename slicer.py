41ce1d9f19f2a5a2e1ba715f62fe383f23ed47a0
Vanessa <vaneguti110@hotmail.com>
2018-12-31 00:29:32 +0100

    Update slicer.py

--- slicer.py
+++ slicer.py
@@ -1,4 +1,4 @@
-#Project to cut/slice a specific word and print the result location of the word using python version 3.7.2
+#Project to cut/slice a specific word called "cali" from the bigword = "supercalifragiligilisticexpialidocious" and display the specific location of the word cali, using python version 3.7.2
 
 
 word = "supercalifragiligilisticexpialidocious"
